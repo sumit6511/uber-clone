d730bf8bff799b9d437199b418a44558e5288098
Sumit Sah <sumitsah6511@gmail.com>
2026-06-03 22:46:12 +0545

    feat(backend): wire /api/users route and connect MongoDB on startup

--- Backend/app.js
+++ Backend/app.js
@@ -2,16 +2,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
-
 const cors = require("cors");
 
 const app = express();
 
+const userRoutes = require("./routes/user.routes");
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/users", userRoutes);
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 const http = require("http");
 const app = require("./app");
 
+const connectDB = require("./db/db");
+
+connectDB();
+
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
